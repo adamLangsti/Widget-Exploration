1ec94e5a7f655f04f4fc092ec07af8dce1bf6707
Adam Lang <Adam.Lang-1@hotmail.com>
2019-05-06 18:09:30 +0200

    Learning widgets

---
+++ app/src/main/java/com/example/widgetexploration/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.widgetexploration;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exploration_layout);
+
+        RadioGroup radioGroup = (RadioGroup)
+        findViewById(R.id.radioGroup);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final Button button = (Button) findViewById(R.id.button);
+        final TextClock tClock = (TextClock) findViewById(R.id.textClock);
+        final CheckBox cbTransparency = (CheckBox) findViewById(R.id.checkBoxTransparency);
+        final CheckBox cbTint = (CheckBox) findViewById(R.id.checkBoxTint);
+        final CheckBox cbReSize = (CheckBox) findViewById(R.id.checkBoxReSize);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setVisibility(View.INVISIBLE);
+
+
+        cbTransparency.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(cbTransparency.isChecked()) {
+                    imageView.setAlpha(.1f);
+                }
+                else{
+                    imageView.setAlpha(1f);
+                }
+            }
+        });
+
+        cbTint.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(cbTint.isChecked()) {
+                    imageView.setColorFilter(Color.argb(150, 255, 0, 0));
+                }else {
+                    imageView.setColorFilter(Color.argb(0, 0, 0, 0));
+                }
+            }
+        });
+
+        cbReSize.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(cbReSize.isChecked()) {
+                    imageView.setScaleX(2);
+                    imageView.setScaleX(2);
+                }else {
+                    imageView.setScaleX(1);
+                    imageView.setScaleX(1);
+                }
+            }
+        });
+
+            radioGroup.clearCheck();
+
+            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    RadioButton rb = (RadioButton)
+                        group.findViewById(checkedId);
+
+                        switch(rb.getId()) {
+
+                            case R.id.radioButtonLondon:
+                                tClock.setTimeZone("Europe/London");
+                                break;
+
+                            case R.id.radioButtonBeijing:
+                                tClock.setTimeZone("CST6CDT");
+                                break;
+
+                            case R.id.radioButtonNewYork:
+                                tClock.setTimeZone("America/New York");
+                                break;
+
+                            case R.id.radioButtonEuropeanEmpire:
+                                tClock.setTimeZone("Europe/Brussels");
+                                break;
+                        }
+                    }
+            });
+
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        textView.setText(editText.getText());
+                    }
+                });
+
+                switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked) {
+                            textView.setVisibility(View.VISIBLE);
+                        }else {
+                            textView.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+                editText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        textView.setText("");
+                    }
+                });
+
+
+    }
+}
+
